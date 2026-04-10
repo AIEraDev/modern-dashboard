56c49ccb1e2b89dc2c5184308565e068556f52bc
AIEraDev <musaabdulkabeer19@gmail.com>
2026-04-10 18:47:15 +0100

    feat: take js little bit forward

---
+++ Javascript course/readme.md
@@ -0,0 +1,1 @@
+[Claude Shared Javascript Curriculum](https://claude.ai/share/68c37962-1991-403d-aa7d-919e92389be4)
--- Javascript course/script.js
+++ Javascript course/script.js
@@ -1,14 +1,25 @@
-// Data Types
-//  - String
-//  - Number
-//  - Boolean - true or false
-//  - Undefined
-//  - Null
-//  - Object
-//  - Array
+// ######### Syntax & data types
 
-const name = "Abdulkabir Musa"; // String
-const matricNumber = "2023/B/SENG/0230"; // String
-const studentId = 30067077; // e.g 300.78 Number
-const isPresent = true; // Boolean
-const hasDriversLicense = false; // Boolean
+// ###### let / const — differences & scoping
+let matric = 6789337;
+const name = "Musa";
+let examDate; // undefined
+
+matric = 5739392;
+
+// ###### Primitive types: string, number, bigint, boolean, null, undefined, symbol
+// const name = "Abdulkabir Musa"; // String
+// const matricNumber = "2023/B/SENG/0230"; // String
+// const studentId = 30067077; // e.g 300.78 Number
+// const isPresent = true; // Boolean
+// const hasDriversLicense = false; // Boolean
+const balance = 10_000_000; // Number
+const age = null; // Empty
+let endDate = undefined; // undefined
+// Big Int
+const a = 4567890987654567898765n;
+const b = BigInt("4567890987654567898765");
+console.log(a);
+console.log(typeof b);
+
+// Type coercion & typeof operator
